3f43e0c316ef3634ec6388ffa8c668bc3dfd0dac
Alexis <alexisvt@gmail.com>
2017-01-19 17:41:20 -0600

    adding configuration file

---
+++ SampleCoreApp/src/SampleCoreApp/Startup.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SampleCoreApp
+{
+    public class Startup
+    {
+        #region Public Fields
+
+        public IConfigurationRoot Configuration { get; set; }
+
+        #endregion Public Fields
+
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        #region Public Methods
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDirectoryBrowser();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.UseFileServer();
+            app.Run( async context =>
+            await context.Response.WriteAsync($"Hello {env.EnvironmentName} it's {Configuration["message"]}"));
+
+        }
+
+        #endregion Public Methods
+    }
+}
